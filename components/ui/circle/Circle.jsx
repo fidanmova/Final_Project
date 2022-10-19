7bf3c2b7420a28e304aa6d0ae386a9dac71b1ffb
natasha8 <nat.parisella@gmail.com>
2022-10-19 11:21:57 +0200

    fix merge and toaster

--- components/ui/circle/Circle.jsx
+++ components/ui/circle/Circle.jsx
@@ -45,12 +45,3 @@ const Circle = () => {
 
 export default Circle;
 
-<iframe
-    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2430.448944248444!2d13.440828016053247!3d52.47100664761096!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a84f9fcdd236b1%3A0x64b7914ccc30ad0a!2sWipperstra%C3%9Fe%2C%2012055%20Berlin!5e0!3m2!1sit!2sde!4v1666171189183!5m2!1sit!2sde"
-    width="600"
-    height="450"
-    style="border:0;"
-    allowfullscreen=""
-    loading="lazy"
-    referrerpolicy="no-referrer-when-downgrade"
-></iframe>;
