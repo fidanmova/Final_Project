5332e511b36e4859df97c416bb2442758baf6600
ba-morgan <ba_morgan@yahoo.co.uk>
2022-10-28 13:46:03 +0200

    Header changes

--- components/ui/dashboard/Board.jsx
+++ components/ui/dashboard/Board.jsx
@@ -4,27 +4,27 @@ import Link from "next/link";
 const Board = ({ data }) => {
   return (
     <div className="w-11/12 lg:w-[65vw] h-[80vh] flex flex-col justify-center items-center  bg-black/50 rounded-xl  shadow-green-500 shadow-md">
-      <h1 className="text-3xl mb-2">dashboard</h1>
-      <div className="w-full flex flex-wrap justify-center">
+      <h1 className="text-xl mb-2">dashboard</h1>
+      <div className="w-full flex flex-wrap justify-center ">
         <DashCard
           text="discover, join or create a circle of developers"
           title="circle"
-          style="text-red-500 shadow-red-500"
+          style="text-red-500 shadow-red-500 hover:scale-95 transition duration-200 ease-in-out"
         />
         <DashCard
           text="check all events around you"
           title="events"
-          style="text-blue-500 shadow-blue-500"
+          style="text-blue-500 shadow-blue-500 hover:scale-95 transition duration-200 ease-in-out"
         />
         <DashCard
           text="chat with your friend and with your circle"
           title="chat"
-          style="text-purple-500 shadow-purple-500"
+          style="text-purple-500 shadow-purple-500 hover:scale-95 transition duration-200 ease-in-out"
         />
         <DashCard
           text="looking for a job? check last listenings"
           title="job"
-          style="text-pink-500 shadow-pink-500"
+          style="text-pink-500 shadow-pink-500 hover:scale-95 transition duration-200 ease-in-out"
         />
       </div>
     </div>
