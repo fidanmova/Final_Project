e34d2ec706c594d9fb9d7ab22794c2308fa4799a
pagoli <oliviapagel89@gmail.com>
2022-11-03 09:55:03 +0100

    merge chat

--- utils/fetcher.js
+++ utils/fetcher.js
@@ -1,40 +1,17 @@
-<<<<<<< HEAD
 export const fetcher = async (...args) => {
-    
-    const response = await fetch(...args);
-    //console.log('response', response);
-    let payload;
-    try {
-        if (response.status === 204)
-            return null;
-        payload = await response.json();
-    } catch (error) {
-        console.error(error);
-    }
-    if (response.ok) {
-        //console.log("response ok");
-        return payload;
-    } else {
-        return Promise.reject(payload.error || new Error(response.message));
-    }
-=======
-export const fetcher = (...args) => {
-  return fetch(...args).then(async (response) => {
-    console.log("response", response);
-    let payload;
-    try {
-      if (response.status === 204) return null;
-      payload = await response.json();
-      console.log("payload", payload);
-    } catch (error) {
-      console.error(error);
-    }
-    if (response.ok) {
-      console.log("response ok");
-      return payload;
-    } else {
-      return Promise.reject(payload.error || new Error(response.message));
-    }
-  });
->>>>>>> olifi-FE
+  const response = await fetch(...args);
+  //console.log('response', response);
+  let payload;
+  try {
+    if (response.status === 204) return null;
+    payload = await response.json();
+  } catch (error) {
+    console.error(error);
+  }
+  if (response.ok) {
+    //console.log("response ok");
+    return payload;
+  } else {
+    return Promise.reject(payload.error || new Error(response.message));
+  }
 };
