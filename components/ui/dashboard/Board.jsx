2447d4281a2962a26f3b2af6383214861d94c572
ba-morgan <ba_morgan@yahoo.co.uk>
2022-11-08 14:43:26 +0100

    latest

--- components/ui/dashboard/Board.jsx
+++ components/ui/dashboard/Board.jsx
@@ -8,27 +8,27 @@ const Board = () => {
         <DashCard
           text="discover, join or create a circle of developers"
           title="circle"
-          style="text-red-500 shadow-red-500 text-center p-2"
+          style="text-red-500 shadow-red-500 text-center p-2 hover:scale-95 transition duration-200 ease-in-out"
         />
         <DashCard
           text="check all events around you"
           title="events"
-          style="text-blue-500 shadow-blue-500 text-center p-2"
+          style="text-blue-500 shadow-blue-500 text-center p-2 hover:scale-95 transition duration-200 ease-in-out"
         />
         <DashCard
           text="chat with your friend and with your circle"
           title="chats"
-          style="text-purple-500 shadow-purple-500 text-center p-2"
+          style="text-purple-500 shadow-purple-500 text-center p-2 hover:scale-95 transition duration-200 ease-in-out"
         />
         <DashCard
           text="looking for a job? check last listenings"
           title="job"
-          style="text-pink-500 shadow-pink-500 text-center p-2"
+          style="text-pink-500 shadow-pink-500 text-center p-2 hover:scale-95 transition duration-200 ease-in-out"
         />
         <DashCard
           text="Discuss your code with your friend"
           title="code"
-          style="text-yellow-500 shadow-yellow-500 w-full h-auto mt-4"
+          style="text-yellow-500 shadow-yellow-500 w-full h-auto mt-4 hover:scale-95 transition duration-200 ease-in-out"
         />
       </div>
     </div>
