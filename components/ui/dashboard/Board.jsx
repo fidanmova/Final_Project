07d1ad3d9d2c771cd02d09d606d35540afdee97a
natasha8 <nat.parisella@gmail.com>
2022-11-14 22:10:25 +0100

    fixed mobile board

--- components/ui/dashboard/Board.jsx
+++ components/ui/dashboard/Board.jsx
@@ -2,7 +2,7 @@ import { DashCard } from "../Card";
 
 const Board = () => {
     return (
-        <div className="w-11/12 lg:w-[65vw] h-[80vh] flex flex-col justify-center items-center  bg-black/50 rounded-xl  shadow-green-500 shadow-md">
+        <div className="w-11/12 lg:w-[65vw] lg:h-[80vh] flex flex-col justify-center items-center  bg-black/50 rounded-xl  shadow-green-500 shadow-md">
             <div className="w-full flex flex-wrap justify-center">
                 <DashCard
                     text="discover, join or create a circle of developers"
