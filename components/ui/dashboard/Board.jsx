8e3abfa7d1e9fdde67eeadc6c5e7c46b09cdf0ad
Byron <94467102+ba-morgan@users.noreply.github.com>
2022-11-17 09:04:13 +0100

    Update Board.jsx

--- components/ui/dashboard/Board.jsx
+++ components/ui/dashboard/Board.jsx
@@ -5,27 +5,27 @@ const Board = () => {
         <div className="w-11/12 lg:w-[65vw] lg:h-[80vh] flex flex-col justify-center items-center  bg-black/50 rounded-xl  shadow-green-500 shadow-md">
             <div className="w-full flex flex-wrap justify-center">
                 <DashCard
-                    text="discover, join or create a circle of developers"
+                    text="Discover, Join or Create a Circle of Developers"
                     title="circle"
                     style="text-red-500 shadow-red-500 text-center p-2 hover:scale-95 transition duration-200 ease-in-out"
                 />
                 <DashCard
-                    text="don't miss it! check all monthly events around you"
+                    text="Don't Miss Out !  Check Events Near You"
                     title="events"
                     style="text-blue-500 shadow-blue-500 text-center p-2 hover:scale-95 transition duration-200 ease-in-out"
                 />
                 <DashCard
-                    text="chat with your friend and with your circle"
+                    text="Chat with Friends & Circle"
                     title="chats"
                     style="text-purple-500 shadow-purple-500 text-center p-2 hover:scale-95 transition duration-200 ease-in-out"
                 />
                 <DashCard
-                    text="looking for a job? check last listenings"
+                    text="Looking for a Job ?  Search latest Listings"
                     title="job"
                     style="text-pink-500 shadow-pink-500 text-center p-2 hover:scale-95 transition duration-200 ease-in-out"
                 />
                 <DashCard
-                    text="Discuss your code with your friend"
+                    text="Code with Friends"
                     title="code"
                     style="text-[#faff00] shadow-[#faff00] w-full mt-2 hover:scale-95 transition duration-200 ease-in-out"
                 />
